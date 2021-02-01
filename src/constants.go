ea35a93b63d31a3c47968212c9be3e2c93e9dace
noskla <a@horsi.fi>
2021-02-01 17:26:06 +0100

    Change one key in ChannelTypes to be more clear

--- src/constants.go
+++ src/constants.go
@@ -8,4 +8,4 @@ var AudioFormats = map[string]uint8{
 // ChannelTypes pseudo-constant declares the names of
 // supported channel types.
 var ChannelTypes = map[string]uint8{
-	"AudioOnly": 1, "StreamOnly": 2, "Mirror": 3}
+	"FilesOnly": 1, "StreamOnly": 2, "Mirror": 3}
