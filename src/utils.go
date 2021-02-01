c6dfa07c301e81851db81d3b5d9f564851cbf2c4
noskla <a@horsi.fi>
2021-02-01 12:38:42 +0100

    Create helper function to handle errors

---
+++ src/utils.go
@@ -0,0 +1,24 @@
+package unkast
+
+import (
+	"fmt"
+	"log"
+)
+
+// HandleError is a common helper function that checks
+// if error has occurred, if it does then check if the
+// error context was fatal, then decide if the program
+// should be halted or not. Returns boolean if error
+// has occurred.
+func HandleError(err error, fatal bool) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+		if fatal {
+			log.Fatalln(err)
+		} else {
+			log.Println(err)
+		}
+		return true
+	}
+	return false
+}
