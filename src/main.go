23797215ac61d020c6e8badaf57b2da599e0b0d1
noskla <a@horsi.fi>
2021-02-01 17:35:03 +0100

    Basic main content, create channel, start http server and start readaudio goroutine

--- src/main.go
+++ src/main.go
@@ -1,5 +1,15 @@
 package unkast
 
+import "log"
+
 func main() {
+
+	channelID, ok := CreateAudioChannel(192, false, AudioFormats["MPEG"], ChannelTypes["FilesOnly"])
+	if !ok {
+		log.Fatalln("Error creating channel")
+	}
+
+	StartHTTPServer("127.0.0.1", "8080")
+	go RoutineReadAudio("/home/alis/Music/Elektronomia - Magic.mp3", channelID.String())
 
 }
